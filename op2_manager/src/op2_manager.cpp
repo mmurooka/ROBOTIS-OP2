71a0a29ea027861b81347155c6509df6ee60ea32
Masaki Murooka <murooka@jsk.imi.i.u-tokyo.ac.jp>
2018-12-16 21:40:18 +0900

    [op2_manager] add op3_joint_trajectory_control_module in op2_manager initialization.

--- op2_manager/src/op2_manager.cpp
+++ op2_manager/src/op2_manager.cpp
@@ -32,6 +32,7 @@
 #include "op3_action_module/action_module.h"
 #include "op2_walking_module/op2_walking_module.h"
 #include "op3_direct_control_module/direct_control_module.h"
+#include "op3_joint_trajectory_control_module/joint_trajectory_control_module.h"
 #include "op3_online_walking_module/online_walking_module.h"
 
 using namespace robotis_framework;
@@ -243,6 +244,7 @@ int main(int argc, char **argv)
   controller->addMotionModule((MotionModule*) HeadControlModule::getInstance());
   controller->addMotionModule((MotionModule*) WalkingModule::getInstance());
   controller->addMotionModule((MotionModule*) DirectControlModule::getInstance());
+  controller->addMotionModule((MotionModule*) JointTrajectoryControlModule::getInstance());
   controller->addMotionModule((MotionModule*) OnlineWalkingModule::getInstance());
 
   // start timer
